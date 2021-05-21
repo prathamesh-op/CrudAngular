33a19d8586810315c155bd7784c8406b2ad81e44
prathamesh-op <67185718+prathamesh-op@users.noreply.github.com>
2021-05-21 19:42:02 +0530

    Add files via upload
    Started angular crud operation

---
+++ src/components/App.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+
+function createCard(contacts){
+  return (
+<Card
+key = {contacts.id}
+name = {contacts.name}
+img = {contacts.imgURL}
+tel = {contacts.phone}
+email = {contacts.email}
+ />
+  );
+}
+
+function App() {
+  return (
+    <div> 
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://pbs.twimg.com/profile_images/744849215675838464/IH0FNIXk.jpg" />
+
+   {contacts.map(createCard)}
+
+
+      {/* <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      /> */}
+    </div>
+  );
+}
+
+export default App;
